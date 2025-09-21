3a2c91f17b0f6ddd67bed9b52478787112b09374
Gurkirat164 <ggurkiratssingh1@gmail.com>
2025-09-21 21:48:41 +0530

    Refactor InputBox component to accept label prop and enhance layout with currency selection

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,27 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <InputBox />
-    </>
+    <div className = "bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 text-orange-500 h-screen w-full flex justify-center items-center">
+      <div className = "border-gray-700 border flex flex-col items-center bg-gray-900 shadow-2xl rounded-3xl w-4/5 sm:max-w-fit h-100">
+        <h1 className = "text-2xl sm:text-3xl my-2 mt-4 font-bold">
+          Currency Converter
+        </h1>
+        <div className = "w-full">
+          <form action="">
+            <div className = "m-2">
+              <InputBox 
+                label = "From"
+              />
+            </div>
+            <div className = "m-2">
+              <InputBox 
+              label = "To"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
-function InputBox() {
+function InputBox({
+      label
+}) {
       return (
-            <div>
-                  Test
+            <div className = "flex">
+                  <div className = "flex flex-col mx-2 sm:mx-3">
+                        <label htmlFor="" className = "mx-1 text-sm font-bold">
+                              {label}
+                        </label>
+                        <input 
+                              type="number"
+                              className = "border-none bg-gray-800 rounded-md p-3 text-white text-lg w-full"
+                        />
+                  </div>
+                  <div className = "flex flex-col mx-3">
+                        <label htmlFor="" className = "mx-1 text-sm font-bold">
+                              Currency
+                        </label>
+                        <select className = "border-none bg-gray-800 rounded-md w-full p-3 text-white text-lg">
+                              <option>
+                                    Test USD
+                              </option>
+                              <option>
+                                    INR
+                              </option>
+                        </select>
+                  </div>
             </div>
       );
 }
